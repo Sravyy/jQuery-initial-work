fdf0a8917a91241ef7bb91bd21c3078630b5444e
sravanthi velaga and victor puentes <sravanthi@>
2017-09-11 15:43:36 -0700

    Basic Images added and defined with jQuery

---
+++ css/dsdfsdf.js
@@ -0,0 +1,65 @@
+var saySomeThing = prompt("Please enter some Sentence : ");
+var firstLast = saySomeThing.charAt(0)+saySomeThing.slice(-1);
+alert(firstLast);
+var finalOutput = firstLast.toUpperCase();
+finalOutput;
+//end of first task
+var saySomeThing = prompt("Please enter some Sentence : ");
+var lastFirst = saySomeThing.slice(-1)+saySomeThing.charAt(0);
+alert(lastFirst);
+var lastFirstOutput = lastFirst.toUpperCase();
+lastFirstOutput;
+//end of second task
+var saySomeThing = prompt("Please enter your favorite color : ");
+var third = function(finalOutput, lastFirstOutput) {
+  alert(finalOutput); alert(lastFirstOutput);
+}
+
+/* end of third task*/
+
+
+
+var saySomeThing = prompt("Please enter some Sentence : ");
+var firstLast = saySomeThing.charAt(0)+saySomeThing.slice(-1);
+alert(firstLast);
+var finalOutput = firstLast.toUpperCase();
+finalOutput;
+
+var lastFirst = saySomeThing.slice(-1)+saySomeThing.charAt(0);
+alert(lastFirst);
+var lastFirstOutput = lastFirst.toUpperCase();
+lastFirstOutput;
+
+var third = function(finalOutput, lastFirstOutput) {
+  alert(finalOutput); alert(lastFirstOutput);
+};
+
+/* end of 4th task*/
+
+
+var saySomeThing = prompt("Please enter some Sentence : ");
+var firstLast = saySomeThing.charAt(0)+saySomeThing.slice(-1);
+alert(firstLast);
+var finalOutput = firstLast.toUpperCase();
+finalOutput;
+
+var lastFirst = saySomeThing.slice(-1)+saySomeThing.charAt(0);
+alert(lastFirst);
+var lastFirstOutput = lastFirst.toUpperCase();
+lastFirstOutput;
+
+var third = function(finalOutput, lastFirstOutput) {
+  alert(finalOutput); alert(lastFirstOutput);
+};
+/* end of 5th task*/
+var saySomeThing = prompt("Please enter some Sentence : ");
+
+var lastFirst = saySomeThing.slice(-1)+saySomeThing.charAt(0);
+var lastFirstOutput = lastFirst.toUpperCase();
+lastFirstOutput;
+var fourth = function(){
+return ((saySomeThing.length / 2).toPrecision(1));
+};
+var finalResult = saySomeThing.charAt(fourth);
+finalResult+saySomeThing+lastFirstOutput;
+/* end of 5th task*/
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("h1").click(function() {
+  alert("This is a Header.");
+  });
+  $("img").click(function() {
+  alert("This is a img.");
+  });
+  $("p").click(function() {
+  alert("This is a p.");
+  });
+});
