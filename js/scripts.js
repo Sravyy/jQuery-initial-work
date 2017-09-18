0656102486caed0ff54764e77e286680ccc2a0d5
saneyee sarkar and sravanthi velaga <sravanthi@>
2017-09-18 12:43:40 -0700

    adding for loop and index of function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-  $("h1").click(function() {
-  alert("This is a Header.");
+
+  var vals = ['h1','img','p'];
+  var tags= ['header', 'image', 'para']
+
+  vals.forEach(function(val){
+    $(val).click(function() {
+    alert("this is a " + tags[vals.indexOf(val)])
+    });
+    // tags.forEach(function(tag){
+    //
+    // });
   });
-  $("img").click(function() {
-  alert("This is a img.");
-  });
-  $("p").click(function() {
-  alert("This is a p.");
-  });
+  // $("h1").click(function() {
+  // alert("This is a Header.");
+  // });
+  // $("img").click(function() {
+  // alert("This is a img.");
+  // });
+  // $("p").click(function() {
+  // alert("This is a p.");
+  // });
 });
